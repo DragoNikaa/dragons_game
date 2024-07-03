f3290572b9170115bb8f08fd680538f58edda665
Julia Jurczak <julia.jurczak15@gmail.com>
2024-07-03 16:14:30 +0200

    feat: add text border class and text borders on start screen

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import pygame
 import game_states
 from button import Button
 from game_states import GameStates
-from text import Text
+from text import Text, TextBorder, BorderPosition
 
 pygame.init()
 screen = pygame.display.set_mode((1280, 720))
@@ -19,16 +19,32 @@ background = pygame.transform.scale(background, (1280, 720))
 
 title_font = pygame.font.Font('fonts/pr_viking.ttf', 200)
 title = Text(title_font, 'DRAGONS', 'white', (1280 // 2, 200))
+title_border_left = TextBorder(title, 'black', 10, BorderPosition.LEFT)
+title_border_right = TextBorder(title, 'black', 10, BorderPosition.RIGHT)
+title_border_top = TextBorder(title, 'black', 10, BorderPosition.TOP)
+title_border_bottom = TextBorder(title, 'black', 10, BorderPosition.BOTTOM)
 
 start_button = Button(380, 180, 'graphics/button_backgrounds/start_game.png', (1280 // 2, 470))
 start_button_text_font = pygame.font.Font('fonts/rurik.ttf', 80)
 start_button_text = Text(start_button_text_font, "START", 'white',
                          (start_button.dest[0], start_button.dest[1] + start_button.height // 6), True,
                          start_button.position)
+start_button_text_border_left = TextBorder(start_button_text, 'black', 4, BorderPosition.LEFT)
+start_button_text_border_right = TextBorder(start_button_text, 'black', 4, BorderPosition.RIGHT)
+start_button_text_border_top = TextBorder(start_button_text, 'black', 4, BorderPosition.TOP)
+start_button_text_border_bottom = TextBorder(start_button_text, 'black', 4, BorderPosition.BOTTOM)
 
-start_screen_elements = pygame.sprite.Group()  # type: pygame.sprite.Group[Union[Text, Button]]
+start_screen_elements = pygame.sprite.Group()  # type: pygame.sprite.Group[Union[Text, TextBorder, Button]]
+start_screen_elements.add(title_border_left)
+start_screen_elements.add(title_border_right)
+start_screen_elements.add(title_border_top)
+start_screen_elements.add(title_border_bottom)
 start_screen_elements.add(title)
 start_screen_elements.add(start_button)
+start_screen_elements.add(start_button_text_border_left)
+start_screen_elements.add(start_button_text_border_right)
+start_screen_elements.add(start_button_text_border_top)
+start_screen_elements.add(start_button_text_border_bottom)
 start_screen_elements.add(start_button_text)
 
 while True:
--- text.py
+++ text.py
@@ -1,3 +1,4 @@
+from enum import Enum
 from typing import Union
 
 import pygame
@@ -5,7 +6,40 @@ import pygame
 
 class Text(pygame.sprite.Sprite):
     def __init__(self, font: pygame.font.Font, text: str, color: Union[str, tuple[int, int, int]],
-                 dest: Union[tuple[int, int], int], antialias: bool = True, position: str = 'center'):
+                 dest: tuple[int, int], antialias: bool = True, position: str = 'center'):
         super().__init__()
+        self.font = font
+        self.text = text
+        self.dest = dest
+        self.antialias = antialias
+        self.position = position
         self.image = font.render(text, antialias, color)
         self.rect = self.image.get_rect(**{position: dest})
+
+
+class BorderPosition(Enum):
+    LEFT = 1
+    RIGHT = 2
+    TOP = 3
+    BOTTOM = 4
+
+
+class TextBorder(pygame.sprite.Sprite):
+    def __init__(self, text_object: Text, color: Union[str, tuple[int, int, int]], thickness: int,
+                 border_position: BorderPosition):
+        super().__init__()
+        self.thickness = thickness
+        self.border_position = border_position
+        self.text_position = text_object.position
+        self.image = text_object.font.render(text_object.text, text_object.antialias, color)
+        self.x, self.y = text_object.dest
+        self.rect = self.rect_with_offset()
+
+    def rect_with_offset(self) -> pygame.Rect:
+        if self.border_position is BorderPosition.LEFT:
+            return self.image.get_rect(**{self.text_position: (self.x - self.thickness, self.y)})
+        if self.border_position is BorderPosition.RIGHT:
+            return self.image.get_rect(**{self.text_position: (self.x + self.thickness, self.y)})
+        if self.border_position is BorderPosition.TOP:
+            return self.image.get_rect(**{self.text_position: (self.x, self.y - self.thickness)})
+        return self.image.get_rect(**{self.text_position: (self.x, self.y + self.thickness)})
