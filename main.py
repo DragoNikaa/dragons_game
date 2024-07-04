7729dacd5ad471b93317c84bd69531785871c532
Julia Jurczak <julia.jurczak15@gmail.com>
2024-07-04 21:54:47 +0200

    feat: scale everything to fullscreen

--- main.py
+++ main.py
@@ -4,13 +4,6 @@ import pygame
 
 import configuration
 
-if __name__ == '__main__':
-    pygame.init()
-    screen = pygame.display.set_mode((configuration.General.SCREEN_WIDTH, configuration.General.SCREEN_HEIGHT))
-    clock = pygame.time.Clock()
-    pygame.display.set_caption(configuration.General.NAME)
-    # pygame.display.set_icon(configuration.General.ICON)
-
 import game_states
 from displayed_elements.start_screen import start_screen_elements
 from displayed_elements.level_selection import level_selection_elements
@@ -25,15 +18,15 @@ def game_loop() -> None:
                 sys.exit()
 
         if game_states.current_state == GameStates.START_SCREEN:
-            start_screen_elements.draw(screen)
+            start_screen_elements.draw(configuration.screen)
             start_screen_elements.update()
 
         elif game_states.current_state == GameStates.LEVEL_SELECTION:
-            level_selection_elements.draw(screen)
+            level_selection_elements.draw(configuration.screen)
             level_selection_elements.update()
 
         pygame.display.update()
-        clock.tick(60)
+        configuration.clock.tick(60)
 
 
 if __name__ == '__main__':
