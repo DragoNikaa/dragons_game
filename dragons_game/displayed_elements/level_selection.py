79ef44646e231fa755cf7ac161ec4291572e1c45
Julia Jurczak <julia.jurczak15@gmail.com>
2024-07-05 22:31:06 +0200

    refactor: simplify displayed elements

--- dragons_game/displayed_elements/level_selection.py
+++ dragons_game/displayed_elements/level_selection.py
@@ -1,26 +1,17 @@
-from dataclasses import dataclass
-
 import pygame.sprite
 
-from dragons_game import background
+from dragons_game.background import Background
 from dragons_game.button import Button
 from dragons_game.configuration import LevelSelection
 from dragons_game.game_states import GameStates
 
+background = Background(LevelSelection.BACKGROUNDS_DIR_PATH)
 
-@dataclass
-class Background:
-    background_elements = background.Background(LevelSelection.BACKGROUNDS_DIR_PATH)
+level_buttons: 'pygame.sprite.Group[Button]' = pygame.sprite.Group()
+for button_number in range(LevelSelection.LEVEL_BUTTONS_NUMBER):
+    button_dest = LevelSelection.LEVEL_BUTTONS_DEST[background.drawn_background_number][button_number]
+    level_buttons.add(
+        Button(LevelSelection.LEVEL_BUTTON_WIDTH, LevelSelection.LEVEL_BUTTON_HEIGHT, LevelSelection.LEVEL_BUTTON_IMAGE,
+               button_dest, GameStates.UNKNOWN))
 
-
-@dataclass
-class LevelButtons:
-    level_buttons_elements: 'pygame.sprite.Group[Button]' = pygame.sprite.Group()
-    for button_number in range(LevelSelection.LEVEL_BUTTONS_NUMBER):
-        button_dest = LevelSelection.LEVEL_BUTTONS_DEST[Background.background_elements.drawn_background_number][
-            button_number]
-        level_buttons_elements.add(Button(LevelSelection.LEVEL_BUTTON_WIDTH, LevelSelection.LEVEL_BUTTON_HEIGHT,
-                                          LevelSelection.LEVEL_BUTTON_IMAGE, button_dest, GameStates.UNKNOWN))
-
-
-level_selection_elements = pygame.sprite.Group(Background.background_elements, LevelButtons.level_buttons_elements)
+level_selection_elements = pygame.sprite.Group(background, level_buttons)
--- dragons_game/displayed_elements/start_screen.py
+++ dragons_game/displayed_elements/start_screen.py
@@ -1,9 +1,6 @@
-from dataclasses import dataclass
-from typing import Any
-
 import pygame
 
-from dragons_game import background
+from dragons_game.background import Background
 from dragons_game.button import Button
 from dragons_game.configuration import StartScreen, General
 from dragons_game.game_states import GameStates
@@ -11,29 +8,17 @@ from dragons_game.text import Text
 
 text_button_text_font = pygame.font.Font(StartScreen.TEXT_BUTTON_TEXT_FONT_PATH, StartScreen.TEXT_BUTTON_TEXT_SIZE)
 
+background = Background(StartScreen.BACKGROUNDS_DIR_PATH)
 
-@dataclass
-class Background:
-    background_elements = background.Background(StartScreen.BACKGROUNDS_DIR_PATH)
+name_font = pygame.font.Font(StartScreen.NAME_FONT_PATH, StartScreen.NAME_SIZE)
+name = Text(name_font, General.NAME.upper(), StartScreen.NAME_COLOR, StartScreen.NAME_DEST, StartScreen.NAME_ANTIALIAS)
+name.add_text_border(StartScreen.NAME_BORDER_COLOR, StartScreen.NAME_BORDER_THICKNESS)
 
+start_button = Button(StartScreen.TEXT_BUTTON_WIDTH, StartScreen.TEXT_BUTTON_HEIGHT, StartScreen.TEXT_BUTTON_IMAGE_PATH,
+                      StartScreen.START_BUTTON_DEST, GameStates.LEVEL_SELECTION)
+start_button.add_text(text_button_text_font, StartScreen.START_BUTTON_TEXT, StartScreen.TEXT_BUTTON_TEXT_COLOR,
+                      StartScreen.TEXT_BUTTON_TEXT_ANTIALIAS, StartScreen.TEXT_BUTTON_Y_OFFSET)
+start_button.add_text_border(StartScreen.TEXT_BUTTON_TEXT_BORDER_COLOR, StartScreen.TEXT_BUTTON_TEXT_BORDER_THICKNESS)
 
-@dataclass
-class Name:
-    name_font = pygame.font.Font(StartScreen.NAME_FONT_PATH, StartScreen.NAME_SIZE)
-    name = Text(name_font, General.NAME.upper(), StartScreen.NAME_COLOR, StartScreen.NAME_DEST,
-                StartScreen.NAME_ANTIALIAS)
-    name.add_text_border(StartScreen.NAME_BORDER_COLOR, StartScreen.NAME_BORDER_THICKNESS)
-
-
-@dataclass
-class StartButton:
-    start_button = Button(StartScreen.TEXT_BUTTON_WIDTH, StartScreen.TEXT_BUTTON_HEIGHT,
-                          StartScreen.TEXT_BUTTON_IMAGE_PATH, StartScreen.START_BUTTON_DEST, GameStates.LEVEL_SELECTION)
-    start_button.add_text(text_button_text_font, StartScreen.START_BUTTON_TEXT, StartScreen.TEXT_BUTTON_TEXT_COLOR,
-                          StartScreen.TEXT_BUTTON_TEXT_ANTIALIAS, StartScreen.TEXT_BUTTON_Y_OFFSET)
-    start_button.add_text_border(StartScreen.TEXT_BUTTON_TEXT_BORDER_COLOR,
-                                 StartScreen.TEXT_BUTTON_TEXT_BORDER_THICKNESS)
-
-
-start_screen_elements: 'pygame.sprite.Group[Any]' = pygame.sprite.Group(
-    Background.background_elements, Name.name, StartButton.start_button)
+start_screen_elements: 'pygame.sprite.Group[Background | Text | Button]' = pygame.sprite.Group(background, name,
+                                                                                               start_button)
