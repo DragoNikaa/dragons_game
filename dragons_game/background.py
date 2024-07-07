6cd44d36ca584dbfb04944d647bc7c8556cbd013
Julia Jurczak <julia.jurczak15@gmail.com>
2024-07-07 19:33:12 +0200

    feat: add bottom tiles to level selection menu

--- dragons_game/background.py
+++ dragons_game/background.py
@@ -7,11 +7,13 @@ from dragons_game import configuration
 
 
 class Background(pygame.sprite.Sprite):
-    def __init__(self, backgrounds_dir_path: str):
+    def __init__(self, backgrounds_dir_path: str, width: int = configuration.General.SCREEN_WIDTH,
+                 height: int = configuration.General.SCREEN_HEIGHT, dest: tuple[int, int] = (0, 0)):
         super().__init__()
+        self.height = height
+        self.dest = dest
         background_paths = [background_path for background_path in pathlib.Path(backgrounds_dir_path).iterdir()]
         self.drawn_background_number = random.randint(1, len(background_paths))
         self.image = pygame.image.load(background_paths[self.drawn_background_number - 1]).convert()
-        self.image = pygame.transform.scale(self.image,
-                                            (configuration.General.SCREEN_WIDTH, configuration.General.SCREEN_HEIGHT))
-        self.rect = self.image.get_rect()
+        self.image = pygame.transform.scale(self.image, (width, height))
+        self.rect = self.image.get_rect(topleft=dest)
--- dragons_game/configuration.py
+++ dragons_game/configuration.py
@@ -53,26 +53,13 @@ class StartScreen:
 
 @dataclass
 class LevelSelection:
-    BACKGROUNDS_DIR_PATH = 'dragons_game/graphics/level_selection_backgrounds'
-
-    LEVEL_BUTTONS_NUMBER = 4
-    LEVEL_BUTTON_WIDTH = LEVEL_BUTTON_HEIGHT = int(General.SCREEN_HEIGHT / 8)
-    LEVEL_BUTTON_IMAGE_PATH = 'dragons_game/graphics/button_backgrounds/level_button.png'
-    LEVEL_BUTTONS_DEST = {
-        1: (
-            (int(General.SCREEN_WIDTH / 5.80), int(General.SCREEN_HEIGHT / 1.95)),
-            (int(General.SCREEN_WIDTH / 2.70), int(General.SCREEN_HEIGHT / 3.70)),
-            (int(General.SCREEN_WIDTH / 1.63), int(General.SCREEN_HEIGHT / 1.80)),
-            (int(General.SCREEN_WIDTH / 1.20), int(General.SCREEN_HEIGHT / 2.70)),
-        )
-    }
-
     TOP_TILES_NUMBER = 4
     TOP_TILE_WIDTH = int(General.SCREEN_WIDTH / TOP_TILES_NUMBER)
     TOP_TILE_HEIGHT = int(General.SCREEN_HEIGHT / 9)
-    TOP_TILE_IMAGE_PATH = 'dragons_game/graphics/menu_elements/tile.png'
+    TILE_BACKGROUND_IMAGE_PATH = 'dragons_game/graphics/menu_elements/tile.png'
     TOP_TILES_DEST = [(x, 0) for x in range(0, General.SCREEN_WIDTH, int(General.SCREEN_WIDTH / TOP_TILES_NUMBER))]
-    TOP_TILES_GAME_STATES = (GameStates.UNKNOWN, GameStates.UNKNOWN, GameStates.UNKNOWN, GameStates.START_SCREEN)
+    TOP_TILE_POSITION = 'topleft'
+    TOP_TILES_GAME_STATES = (GameStates.UNKNOWN, GameStates.UNKNOWN, GameStates.UNKNOWN, GameStates.UNKNOWN)
 
     TOP_TILES_IMAGE_WIDTHS = (
         int(TOP_TILE_HEIGHT / 1.6), int(TOP_TILE_HEIGHT / 2.1), int(TOP_TILE_HEIGHT / 1.5), int(TOP_TILE_HEIGHT / 1.85))
@@ -90,3 +77,48 @@ class LevelSelection:
 
     TOP_TILE_TEXT_BORDER_COLOR = 'black'
     TOP_TILE_TEXT_BORDER_THICKNESS = int(TOP_TILE_TEXT_SIZE / 30)
+
+    BOTTOM_TILES_NUMBER = 5
+    BOTTOM_TILE_WIDTH = int(General.SCREEN_WIDTH / BOTTOM_TILES_NUMBER)
+    BOTTOM_TILE_HEIGHT = int(General.SCREEN_HEIGHT / 7)
+    BOTTOM_TILES_DEST = [(x, General.SCREEN_HEIGHT) for x in
+                         range(0, General.SCREEN_WIDTH, int(General.SCREEN_WIDTH / BOTTOM_TILES_NUMBER))]
+    BOTTOM_TILE_POSITION = 'bottomleft'
+    BOTTOM_TILES_GAME_STATES = (
+        GameStates.UNKNOWN, GameStates.UNKNOWN, GameStates.UNKNOWN, GameStates.UNKNOWN, GameStates.UNKNOWN)
+
+    BOTTOM_TILES_IMAGE_WIDTHS = (
+        int(BOTTOM_TILE_HEIGHT / 1.85), int(BOTTOM_TILE_HEIGHT / 1.85), int(BOTTOM_TILE_HEIGHT / 1.85),
+        int(BOTTOM_TILE_HEIGHT / 1.85), int(BOTTOM_TILE_HEIGHT / 1.85))
+    BOTTOM_TILE_IMAGE_HEIGHT = int(BOTTOM_TILE_HEIGHT / 1.85)
+    BOTTOM_TILES_IMAGE_PATHS = (
+        'dragons_game/graphics/menu_elements/coin.png', 'dragons_game/graphics/menu_elements/coin.png',
+        'dragons_game/graphics/menu_elements/map.png', 'dragons_game/graphics/menu_elements/coin.png',
+        'dragons_game/graphics/menu_elements/coin.png')
+    BOTTOM_TILE_IMAGE_X_OFFSET = -int(BOTTOM_TILE_WIDTH / 3.3)
+
+    BOTTOM_TILE_TEXT_FONT_PATH = 'dragons_game/fonts/pr_viking.ttf'
+    BOTTOM_TILE_TEXT_SIZE = int(BOTTOM_TILE_HEIGHT / 3)
+    BOTTOM_TILES_TEXTS = ('Hatchery', 'Dragons', 'Islands', 'Market', 'Settings')
+    BOTTOM_TILE_TEXT_COLOR = 'white'
+    BOTTOM_TILE_TEXT_ANTIALIAS = True
+    BOTTOM_TILE_TEXT_X_OFFSET = int(BOTTOM_TILE_WIDTH / 8)
+
+    BOTTOM_TILE_TEXT_BORDER_COLOR = 'black'
+    BOTTOM_TILE_TEXT_BORDER_THICKNESS = TOP_TILE_TEXT_BORDER_THICKNESS
+
+    BACKGROUNDS_DIR_PATH = 'dragons_game/graphics/level_selection_backgrounds'
+    BACKGROUND_HEIGHT = General.SCREEN_HEIGHT - TOP_TILE_HEIGHT - BOTTOM_TILE_HEIGHT
+    BACKGROUND_DEST = (0, TOP_TILE_HEIGHT)
+
+    LEVEL_BUTTONS_NUMBER = 4
+    LEVEL_BUTTON_WIDTH = LEVEL_BUTTON_HEIGHT = int(BACKGROUND_HEIGHT / 6.5)
+    LEVEL_BUTTON_IMAGE_PATH = 'dragons_game/graphics/button_backgrounds/level_button.png'
+    LEVEL_BUTTONS_DEST = {
+        1: (
+            (int(General.SCREEN_WIDTH / 5.80), TOP_TILE_HEIGHT + int(BACKGROUND_HEIGHT / 1.95)),
+            (int(General.SCREEN_WIDTH / 2.70), TOP_TILE_HEIGHT + int(BACKGROUND_HEIGHT / 3.70)),
+            (int(General.SCREEN_WIDTH / 1.63), TOP_TILE_HEIGHT + int(BACKGROUND_HEIGHT / 1.80)),
+            (int(General.SCREEN_WIDTH / 1.20), TOP_TILE_HEIGHT + int(BACKGROUND_HEIGHT / 2.70)),
+        )
+    }
